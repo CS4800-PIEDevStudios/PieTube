0a79f7c439d54bcf49460823f635b67c2a4f373d
Plutzz <benjiongco@gmail.com>
2025-02-19 00:29:25 -0800

    Updates

---
+++ assets/Page1-CIRvskPi.js
@@ -0,0 +1,1 @@
+import{j as i,L as s}from"./index-KyEbHcXy.js";import{B as e}from"./Button-CSslowVK.js";const t=()=>i.jsxs("div",{children:[i.jsx("h1",{children:"Page 1"}),i.jsx("p",{children:"This is Page 1."}),i.jsx(s,{to:"/PieTube",children:i.jsx(e,{variant:"primary",children:" Home "})})]});export{t as default};
