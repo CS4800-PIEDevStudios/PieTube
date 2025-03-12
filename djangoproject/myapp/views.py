4b65b09ecd07c315d94fc5417ad8d114bf4f8f7b
Plutzz <benjiongco@gmail.com>
2025-03-12 10:13:46 -0700

    Change connection package
    From this point forward we will be using django.connection instead of mysql.connection to connect to the database.

--- djangoproject/myapp/views.py
+++ djangoproject/myapp/views.py
@@ -1,25 +1,16 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import mysql.connector
+from django.db import connection
+
 
 def getMovieData(request):
-	# Connect to server
-	cnx = mysql.connector.connect(
-		host="pietube-database.cwl0gw24qtjq.us-east-1.rds.amazonaws.com",
-		port=3306,
-		user="admin",
-		password="OvCCanRNLvBjdfiKUt3I")
 
-	# Get a cursor
-	cur = cnx.cursor(dictionary=True)
 
-	cur.execute("SELECT * FROM PieTube.Movie")
-
-	result = cur.fetchall()
-
-	cnx.close()
-	cur.close()
-
+	with connection.cursor() as cur:
+		cur.execute("SELECT * FROM PieTube.Movie")
+		result = cur.fetchall()
+		
 	print(result)
 
 	return JsonResponse(result, safe=False)
