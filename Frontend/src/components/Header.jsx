04f6fb375230c7949f947f4ae08f4c450ba37419
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-03-13 22:50:03 -0700

    e

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -10,7 +10,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 
 const Header = () => {
     return (
-      <Navbar bg="light" data-bs-theme="light" className='w-100 shadow-lg mb-5 p-4 justify-content-between'>
+      <Navbar bg="light" data-bs-theme="light" className='w-100 shadow-lg mb-5 px-5 justify-content-between'>
         <Navbar.Brand>
           <Link to="/PieTube" >
               <img src={pietubelogo} className='mr-3' style={{maxHeight: "40px"}}/> 
@@ -18,11 +18,14 @@ const Header = () => {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 					<Nav>
-						<Nav.Link className='mx-5 text-nowrap text-decoration-none'> <Link to="/PieTube" style={{color:'#000000', fontSize:'1.5rem'}}> Home </Link> </Nav.Link>
-            <Nav.Link className='mx-5 text-nowrap text-decoration-none'> <Link to="/Login" style={{color:'#000000', fontSize:'1.5rem'}}> Trending </Link> </Nav.Link>
-            <Nav.Link className='mx-5 text-nowrap text-decoration-none'> <Link to="/Signup" style={{color:'#000000', fontSize:'1.5rem'}}> Watch List </Link> </Nav.Link>
+            <div className='d-flex text-nowrap' style={{gap:"50px"}}> 
+              <Nav.Link> <Link to="/PieTube" style={{color:'#000000', fontSize:'1.5rem'}}> Home </Link> </Nav.Link>
+              <Nav.Link> <Link to="/Login" style={{color:'#000000', fontSize:'1.5rem'}}> Trending </Link> </Nav.Link>
+              <Nav.Link> <Link to="/Signup" style={{color:'#000000', fontSize:'1.5rem'}}> Watch List </Link> </Nav.Link>
+            </div>
+						
 					</Nav>
-          <InputGroup className="ml-5 w-50">
+          <InputGroup className="ml-5 w-25">
             <Form.Control
               placeholder="Search"
               aria-label="Search"
@@ -30,7 +33,7 @@ const Header = () => {
               style={{backgroundColor:'#E1E1E1', borderRadius:'10px', border:'none', height:'50px'}}
             />
           </InputGroup>
-          <div className='mx-5 rounded-circle'style={{ width:'90px', height:'90px', overflow:'hidden'}}>
+          <div className='mx-5 rounded-circle'style={{ width:'80px', height:'80px', overflow:'hidden'}}>
             <Link to="/Profile" >
                 <Image src={mepic} style={{ width:'100%', height:'100%', objectFit:'cover' }}/>
             </Link>
