b1a17126d5dcc46a147907738dd16d3ce5c929f3
Plutzz <benjiongco@gmail.com>
2025-03-20 23:50:07 -0700

    Update settings.py

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -58,7 +58,7 @@ CORS_ORIGIN_ALLOW_ORIGIN = True
 
 CORS_ALLOW_ALL_ORIGINS = True
 
-CSRF_COOKIE_SECURE = False  # Disable if not using HTTPS in development
+CSRF_COOKIE_SECURE = True  # Disable if not using HTTPS in development
 CSRF_COOKIE_HTTPONLY = False  # Allow JavaScript access to the CSRF cookie
 CSRF_COOKIE_SAMESITE = 'Lax'  # Allow cookies in cross-origin requests
 
