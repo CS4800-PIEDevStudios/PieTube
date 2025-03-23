25e1a28146dbe22a975b6e9002807b0a73f3f99b
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-03-23 03:21:52 -0700

    movie player

--- Frontend/src/components/MoviePlayer.jsx
+++ Frontend/src/components/MoviePlayer.jsx
@@ -1,23 +1,49 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Ratio } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 
 const MoviePlayer = () => {
-    // Navigation hook
-    const navigate = useNavigate(); 
 
     return (
-        <div className='d-flex flex-column w-50'> 
-            <Ratio id='MoviePlayer' aspectRatio={"16x9"}>
+        <div className='d-flex flex-column w-100'> 
+            <div id='MoviePlayer' className='align-self-center w-50'>
                 <iframe
-                        title="Movie Player"
-                        allowFullScreen={true}
-                        src="https://vidsrc.dev/embed/movie/tt29623480"
-                        style={{ width: '100%', height: '800px', border: 'none' }}
-                    ></iframe>
-            </Ratio>
-            <div id='MoviePlayerDescription'>
+                    title="MoviePlayer"
+                    allowFullScreen={true}
+                    className='movie-player'
+                    src="https://vidsrc.dev/embed/movie/tt9362722"
+                    style={{boxShadow: "0px 0px 80px rgba(0, 0, 0, 1)"}}
+                ></iframe>
+            </div>
 
+            <hr/>
+
+            <div id='MoviePlayerDescription' className='d-flex my-5 bg-secondary w-100 justify-content-between p-5'>
+                <div className='d-flex flex-column align-items-start'>
+                    <h1 id='Title'> Spider-Man: Across the Spider-Verse </h1>
+                    <div id='Genres' className='d-flex flex-wrap'>
+                        {Array.from({ length: 10 }).map((_, index) => (
+                            <div key={index} className='movie-player-genre-blob'>
+                                Genre
+                            </div>
+                        ))} 
+                    </div>
+                    <div className='hr'/>
+                    <div id='Contributors'>
+                        <div id='Directors' className='d-flex m-3 justify-content-between align-text-bottom gap-3'> 
+                            <h3>Directors</h3>
+                            <div className='d-flex align-text-bottom'>
+                                {Array.from({ length: 3 }).map((_, index) => (
+                                    <p className='mx-3'>name</p>
+                                ))}
+                            </div>
+
+                        </div>
+                        <div id='Writers'></div>
+                    </div>
+                </div>
+                <div>
+
+                </div>
             </div>
         </div>
     );
