68ec8e942b2e1f79396317b2f113bc8cb4e8da5f
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-03-23 02:32:49 -0700

    movie player

--- Frontend/src/components/MoviePlayer.jsx
+++ Frontend/src/components/MoviePlayer.jsx
@@ -1,13 +1,24 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { Ratio } from 'react-bootstrap';
 
 const MoviePlayer = () => {
     // Navigation hook
     const navigate = useNavigate(); 
 
     return (
-        <div>
-            <h1>Movie Player page</h1>
+        <div className='d-flex flex-column w-50'> 
+            <Ratio id='MoviePlayer' aspectRatio={"16x9"}>
+                <iframe
+                        title="Movie Player"
+                        allowFullScreen={true}
+                        src="https://vidsrc.dev/embed/movie/tt29623480"
+                        style={{ width: '100%', height: '800px', border: 'none' }}
+                    ></iframe>
+            </Ratio>
+            <div id='MoviePlayerDescription'>
+
+            </div>
         </div>
     );
 };
