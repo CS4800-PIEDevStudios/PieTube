8a6f4a580df1877f3d74ea24ab8e449900adb13d
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-03-24 01:02:11 -0700

    Buttons and Search results page

---
+++ Frontend/src/components/SearchResults.jsx
@@ -0,0 +1,18 @@
+
+const SearchResults = () => {
+    return (
+        <div className="d-flex flex-column w-50 pt-5" style={{height:"100vh"}}>
+            {/* Header */}
+            <div className="d-flex flex-column align-self-start align-items-start">
+                <h1>Results for:</h1>
+                <h3 className="text-muted">Spider-man: Across the Spider-Verse</h3>
+            </div>
+            {/* Results */}
+            <div>
+                
+            </div>
+        </div>
+    );
+};
+
+export default SearchResults;
