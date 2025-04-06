8b33ad42d4a597491567fca5a1780f09e7750513
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-04-05 22:05:24 -0700

    Test

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -36,6 +36,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     'http://localhost:5174',
     'http://localhost:5175',
+    "http://localhost:5178",
     'http://localhost:8000',
 	'http://127.0.0.1:8000',
     'https://cs4800-piedevstudios.github.io',
@@ -61,6 +62,7 @@ CSRF_TRUSTED_ORIGINS = [
 	'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    "http://localhost:5178",
     'http://localhost:8000',
     'https://cs4800-piedevstudios.github.io',
     'https://ec2-23-20-205-143.compute-1.amazonaws.com'
