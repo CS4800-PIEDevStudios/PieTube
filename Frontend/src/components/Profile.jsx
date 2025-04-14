167bf48951b16ff09a9c6d28dea8814e1635e0a5
KALE <114507504+kaleyvuong@users.noreply.github.com>
2025-04-14 11:37:29 -0700

    Organize profile page

--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -9,44 +9,22 @@ const Profile = () => {
     const [username, setUsername] = useState("");
     const [about, setAbout] = useState("");
     const [isLoading, setIsLoading] = useState(true); 
+    const [profilePicUrl, setProfilePicUrl] = useState(null);
 
     // Navigation hook
     const navigate = useNavigate(); 
 
-    //File input for profile
-    const [profilePicUrl, setProfilePicUrl] = useState(null);
+    // File input for profile
     const fileInputRef = useRef(null);
 
-    const handleSignOut = async(event) => {
-        event.preventDefault();
-        const response = await axiosInstance.post('login-api/logoutAccount', {});
-        console.log('Response from Django Signout:', response.data);
-        if (response.data.status === 'success') {
-            localStorage.removeItem('isAuthenticated');
-            localStorage.setItem("isLoggedIn", false);
-            navigate("/Login");
-            window.location.reload();
-        }
-    };
-
-    const handleFileChange = (event) => {
-        const file = event.target.files[0];
-        if (file) {
-            const reader = new FileReader();
-            reader.onloadend = () => {
-                setProfilePicUrl(reader.result);
-            };
-            reader.readAsDataURL(file);
-        }
-    };
-
-  useEffect(() => {
+    useEffect(() => {
         if(localStorage.getItem('isLoggedIn') === 'false')
         {
             navigate("/Login");
             return;
         }
         
+        // Fetches profile data
         axiosInstance.get('login-api/getProfileData')
         .then(response => {
             console.log(response.data[0])
@@ -64,22 +42,46 @@ const Profile = () => {
                 setIsLoading(false);
             });
     }, [navigate]);
-   
+
+    // Function for signing out
+    const handleSignOut = async(event) => {
+        event.preventDefault();
+        const response = await axiosInstance.post('login-api/logoutAccount', {});
+        console.log('Response from Django Signout:', response.data);
+        if (response.data.status === 'success') {
+            localStorage.removeItem('isAuthenticated');
+            localStorage.setItem("isLoggedIn", false);
+            navigate("/Login");
+            window.location.reload();
+        }
+    };
+
+    // Function for changing profile picture
+    const handleFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setProfilePicUrl(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     const handleProfilePicClick = () => {
         fileInputRef.current.click();
     };
 
+    // Loading screen
     if (isLoading) {
         return <div>Loading profile...</div>;
     }
-
 
     return (
         <div className="justify-content-center p-5 bg-light mt-5" style={{minWidth:"500px", boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.25)", borderRadius: "20px"}}>
             {/* Header */}
             <h1 className="text-center mb-4">Settings</h1>
             <div className="d-flex align-items-center mb-4">
-            
                 {/* Profile Picture  */}
                 <div
                     onClick={handleProfilePicClick}
@@ -90,7 +92,6 @@ const Profile = () => {
                         <img
                             src={profilePicUrl}
                             alt="Profile Preview"
-                            
                         />
                     ):(
                         <span>Change <br /> Profile <br /> Picture</span> 
@@ -106,7 +107,6 @@ const Profile = () => {
                     accept="image/*"
                 />
                 {/* End Profile Picture Area */}
-
                 <div className="ms-3" style={{ minWidth: 0 }}></div>
                 <h5 className="text-start mx-3">Username:<br></br> {username}</h5>
                 <h5 className="flex-fill">Email:<br></br> {email}</h5>
